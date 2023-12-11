38c2e768b2fc924b01521b647e623f0a1d0dda1e
Mramar-croatia <lobelmarunic@gmail.com>
2023-12-11 22:51:50 +0100

    Prepared for working with pandas and matplotlib.

--- imrpoved_main.py
+++ imrpoved_main.py
@@ -1,6 +1,6 @@
 from tkinter import Tk
 from tkinter import filedialog as fd
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -17,6 +17,18 @@ def process_transcript():
     modify_transcript(input_path, output_path)
 
 
-lst = ["ja", "Ba", "ja"]
-df = pd.DataFrame(lst)
+data_dict = {'age': [20, 20, 21, 20, 21, 20],
+             'math_marks': [1, 90, 91, 98, 92, 95],
+             'physics_marks': [90, 100, 91, 92, 98, 95],
+             'chem_marks': [93, 89, 99, 92, 94, 92]
+             }
+
+df = pd.DataFrame(data_dict, index=['Marko', 'Jana', 'Sara', 'Fara', 'Klara', 'Maja'])
+
+plot = df.plot.pie(y='math_marks', figsize=(10, 10), fontsize = 12)
+
+plt.title('Math plot')
+
+plt.show()
+
 print(df)
